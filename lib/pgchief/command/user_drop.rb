ad22808abdf5b27775296e887f6b0e56e176d12d
Joel Oliveira <joel.oliveira@gmail.com>
2024-08-30 20:59:19 -0400

    Revoke privs before dropping user
    Otherwise, you try to drop the user when they have access to databases and it'll complain loudly.
    
    This will loop through all the databases a user has access to, revoke access, _then_ drop the user.

--- lib/pgchief/command/user_drop.rb
+++ lib/pgchief/command/user_drop.rb
@@ -10,7 +10,8 @@ module Pgchief
         @username = params.first
         raise Pgchief::Errors::UserExistsError unless user_exists?
 
-        conn.exec("DROP USER #{username}")
+        revoke_all_privileges
+        drop_user
 
         "User '#{username}' dropped successfully!"
       rescue PG::Error => e
@@ -19,9 +20,34 @@ module Pgchief
         conn.close
       end
 
+      private
+
       def user_exists?
         query = "SELECT 1 FROM pg_user WHERE usename = '#{username}'"
         conn.exec(query).any?
+      end
+
+      def revoke_all_privileges
+        databases_with_access.each do |database|
+          conn.exec("REVOKE ALL PRIVILEGES ON DATABASE #{database} FROM #{username};")
+        end
+      end
+
+      def drop_user
+        conn.exec("DROP USER #{username}")
+      end
+
+      def databases_with_access
+        @databases_with_access ||= begin
+          results = conn.exec <<~SQL
+            SELECT datname
+            FROM pg_database
+            WHERE has_database_privilege('#{username}', datname, 'CONNECT')
+              AND datname NOT IN ('postgres', 'template1', 'template0')
+          SQL
+
+          results.map { |row| row["datname"] }
+        end
       end
     end
   end
